ad8633eb04bb6da01e3331a9de4388f865825572
tnum <teenum@susumu.co.uk>
2018-01-20 21:58:13 +0000

    Saturday push

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import getWeb3 from './utils/getWeb3'
 import AppBar from 'material-ui/AppBar'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+// import darkBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import RaisedButton from 'material-ui/RaisedButton'
+import Paper from 'material-ui/Paper'
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -62,35 +65,39 @@ class App extends Component {
     })
   }
 
-  render() {
-    // return (
-    //   <div className="App">
-    //     <nav className="navbar pure-menu pure-menu-horizontal">
-    //         <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-    //     </nav>
+  
 
-    //     <main className="container">
-    //       <div className="pure-g">
-    //         <div className="pure-u-1-1">
-    //           <h1>Good to Go!</h1>
-    //           <p>Your Truffle Box is installed and ready.</p>
-    //           <h2>Smart Contract Example</h2>
-    //           <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-    //           <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-    //           <p>The stored value is: {this.state.storageValue}</p>
-    //         </div>
-    //       </div>
-    //     </main>
-    //   </div>
-    // );
+  render() {
+    const style = {
+      height: 250,
+      width: 250,
+      margin: 20,
+      textAlign: 'center',
+      display: 'inline-block',
+      verticalAlign: 'top',
+    };
     return (
-      <div className="App">
-        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-          <AppBar title="Access Allow" />
+        <MuiThemeProvider>
+          <div>
+            <AppBar title="Access Allow" />
+            <div>
+              <div>
+                <Paper style={style} zDepth={1}>
+                  <RaisedButton label="Primary" primary={true} />
+                </Paper>
+                <Paper style={style} zDepth={1}/>
+                
+                </div>
+                <div>
+                <Paper style={style} zDepth={1}/>
+                <Paper style={style} zDepth={1}/>
+                
+                </div>
+            </div>
+            <div className="right-side"></div>
+          </div>
         </MuiThemeProvider>
-      </div>
     );
   }
 }
-
 export default App
